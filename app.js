e026d938b1c15d6f03a7153ece66750f17493853
francis balimuttajjo <fbalimuttajjo@gmail.com>
2022-04-30 15:50:39 +0300

    added server file

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const db = require("./models");
 const itemRouter = require("./routes/item");
 const clientRouter = require("./routes/client");
 const salesRouter = require("./routes/sale");
 const transactionsRouter = require("./routes/transaction");
+
 
 app.use(express.json());
 
@@ -14,11 +14,6 @@ app.use("/api/v1", salesRouter);
 app.use("/api/v1", transactionsRouter);
 
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, async () => {
-  console.log(`listening  on ${PORT}`);
-  db.sequelize
-    .authenticate()
-    .then(() => console.log("authenticated"))
-    .catch((err) => console.log(err));
-});
+
+
+module.exports =app
---
+++ server.js
@@ -0,0 +1,12 @@
+const db = require("./models");
+const app = require("./app");
+
+const PORT = process.env.PORT || 8000;
+//connecting to postgress db using sequelize
+app.listen(PORT, async () => {
+  console.log(`listening  on ${PORT}`);
+  db.sequelize
+    .authenticate()
+    .then(() => console.log("authenticated"))
+    .catch((err) => console.log(err));
+});

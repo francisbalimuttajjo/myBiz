a2ee8d906dc988f526fef54631134e988dec462d
francis balimuttajjo <fbalimuttajjo@gmail.com>
2022-05-14 13:01:11 +0300

    protected sales route

--- routes/sale.js
+++ routes/sale.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const salesController = require("../controllers/sale");
+const authController = require("../controllers/auth");
 const router = express.Router();
 
 router
   .route("/sales")
-  .post(salesController.addOneSale);
+  .post(authController.isAuthenticated, salesController.addOneSale);
 
-router.route("/sales/getAll").post(salesController.getAllSales);
+router
+  .route("/sales/getAll")
+  .post(authController.isAuthenticated, salesController.getAllSales);
 
 router
   .route("/sales/:id")
-  .delete(salesController.deleteOneSale);
+  .delete(authController.isAuthenticated, salesController.deleteOneSale);
 
-router.route("/sales/reverse/:id").delete(salesController.reverseSale);
+router
+  .route("/sales/reverse/:id")
+  .delete(authController.isAuthenticated, salesController.reverseSale);
 module.exports = router;
